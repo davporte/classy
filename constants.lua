a502ef3cce38cce0c2afc0d8d3a7f145c33904ef
David Porter <57982995+davporte@users.noreply.github.com>
2019-12-26 14:59:20 +0800

    Update constants.lua
    Improved constants.lua by allowing trickle down of immutabliity across all future entries

--- constants.lua
+++ constants.lua
@@ -1,5 +1,5 @@
 local constants = {
-	 _VERSION = ... .. '.lua 1.1.1',
+	 _VERSION = ... .. '.lua 1.1.2',
      _URL = 'https://github.com/davporte/classy/blob/master/constants.lua',
      _DESCRIPTION = [[
       ============================================================================
@@ -47,6 +47,9 @@ local constants = {
       _DEPENDANCIES = { 'classy','inspect' },
 }
 
+-- a local function place holder
+local mettaGetta
+
 -- require dependancies if any
 local dependancies = constants._DEPENDANCIES
 if dependancies then
@@ -62,6 +65,89 @@ if dependancies then
 	end
 end
 
+--- this returns a key stored in a tables metatable, we are doing this to forc __index/__newindex to control the attributes
+-- @param t the table we want the  result from
+-- @param k the particular key we want from t's metatable 
+-- @return the value of key k in table t metatable 
+local function indexLocal ( t, k )
+	return getmetatable ( t ) [ k ] 
+end
+
+--- sets a key in a tables metatable, we are doing this to forc __index/__newindex to control the attributes.dependancies
+-- If the key exists then an exception will be raised, as constants are immutable
+-- @param t the table we want to set the value in ( it goes in its metatable )
+-- @param k the particular key we want to set in t's metatable 
+-- @param v the value we want to set the key to
+-- @param path is text showing the attributes table structure, for error message
+-- @param depth how far down in the stack are we for an error message
+-- @return None 
+local function newindexLocal ( t, k, v, depth, path )
+	local mt = getmetatable ( t )
+	if not mt [ k ] then
+		mt [ k ] = mettaGetta ( v, depth, path .. '.' .. k )
+	else
+		error ( 'key ' .. path .. '.' .. k .. ' is immutable, and cannot be changed in ' .. constants._MODULENAME .. ' once it is set', depth )
+	end
+end
+
+--- gets an attribute from the secretstore
+-- @param obj the object
+-- @param t the table we want the result from ( ignored as we are using secretStore )
+-- @param k the particular key we want from the secretStore
+-- @return the value from the secretStore or nil
+local function gettingAttribute ( obj, t, k )
+	if obj [ k ] then 
+		return obj [ k ] 
+	elseif obj.secretStore then
+		return obj.secretStore [ k ] 
+	end 
+end
+
+
+--- takes an attribute and if it is a table also makes that table immutable
+-- @function metaGetta
+-- @param v the value of attribute being set
+-- @param depth how deep are we into the stack for error messages
+-- @param path is text showing the attributes table structure, for error message
+-- @return the value of the attribute, and if this is atable the approriate metatable is also attached
+mettaGetta =  function ( v, depth, path )
+	depth = depth + 1
+	if type ( v ) == constants._TABLETYPE then -- attach we want this immutablity to propogte down through tables
+		local mt = {
+				__index = indexLocal,
+				__newindex = function ( t, k, v ) newindexLocal ( t, k, v, depth, path ) end
+		}
+		-- move values over to the local metatable
+		local next = next
+		local key, val
+		for key, val in next, v, nil do
+			mt [ key ] = mettaGetta ( val, depth, path )
+			v [ key ] = nil
+		end
+		return setmetatable ( v, mt )
+	else
+		return v
+	end
+end
+
+--- sets atributes for a table, itterates through that table locking it down from change
+-- @param obj the object making the call
+-- @param t the table the attribute is being set
+-- @param k the key we are setting
+-- @param v the value we are setting
+local function settingAttribute ( obj, t, k, v )
+	if not (obj.locked or obj.fortress) then -- if we have not locked the store
+		if not obj.secretStore [ k ] then -- if we have not already set the attribute, as we want attributes to be immutable			
+			obj.secretStore [ k ] = mettaGetta ( v, 1, k )  
+		else
+			-- throw error +3, settingAttribute, __newindex, module
+			error ( 'key ' .. k .. ' is immutable, and cannot be changed in ' .. constants._MODULENAME .. ' once it is set', 3 )
+		end
+	else 
+		-- throw error +3, settingAttribute, __newindex, module
+		error ('store is locked in ' .. constants._MODULENAME, 3 ) 
+	end 
+end
 
 -- returns the class that deals with immutable constants
 return classy:newClass (  
@@ -84,21 +170,10 @@ return classy:newClass (
 						-- we also have to test for obj values first before assumeing it is a secretStore Value
 						-- bindMetaMethods expect the parameters t, k and optional v. We need to bind these methods so that __index/__newindex are viewed as running inside a constructor method.
 						obj.attributeStore.__index = classy:bindMetaMethod ( obj, function ( t, k )  
-																					if obj [ k ] then return obj [ k ] 
-																					elseif obj.secretStore then
-																						return obj.secretStore [ k ] 
-																						end 
+																						return gettingAttribute ( obj, t, k )
 																					end )
 						obj.attributeStore.__newindex = classy:bindMetaMethod ( obj, function ( t, k, v ) 
-																						if not (obj.locked or obj.fortress) then -- if we have not locked the store
-																							if not obj.secretStore [ k ] then -- if we have not already set the attribute, as we want attributes to be immutable
-																								obj.secretStore [ k ] = v  
-																							else
-																								error ( tostring ( k ) .. ' attribute, once set remain immutable', 5)
-																							end
-																						else 
-																							error ('store is locked', 5 ) 
-																						end 
+																						settingAttribute ( obj, t, k, v )
 																					end ) 
 						setmetatable (obj.attributeStore, obj.attributeStore)
 					end
